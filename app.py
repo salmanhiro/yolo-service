7c19654b360fd7326c80edf05648ed97cbec397f
salmanhiro <salman.consulting@outlook.com>
2021-08-16 03:00:59 +0700

    add repo url

--- app.py
+++ app.py
@@ -12,9 +12,7 @@ def object_main():
 
     st.write('Note: I do not store your image since I am not planning to invest on storage. I am broke')
     st.write("\n")
-    st.write("References: ")
-    st.markdown("`Redmon, Joseph, and Ali Farhadi. Yolov3: An incremental improvement. arXiv preprint arXiv:1804.02767 (2018). https://pjreddie.com/darknet/yolo/`")
-    st.markdown("`Gupta, Srishti. Object Detection App using YOLOv3, OpenCV and Streamlit. https://srishti.hashnode.dev/object-detection-app-using-yolov3-opencv-and-streamlit-1`")
+
     choice = st.radio("", ("Show Sample", "Browse your Image"))
 
     if choice == "Browse your Image":
@@ -30,6 +28,13 @@ def object_main():
         our_image = Image.open("images/sampleimg.jpg")
         detect_objects(our_image)
 
+    
+    st.write("References: ")
+    st.markdown("`Redmon, Joseph, and Ali Farhadi. Yolov3: An incremental improvement. arXiv preprint arXiv:1804.02767 (2018). https://pjreddie.com/darknet/yolo/`")
+    st.markdown("`Gupta, Srishti. Object Detection App using YOLOv3, OpenCV and Streamlit. https://srishti.hashnode.dev/object-detection-app-using-yolov3-opencv-and-streamlit-1`")
+
+    st.write("Repository")
+    st.markdown("`https://github.com/salmanhiro/yolo-service`")
     
 
 if __name__ == '__main__':
